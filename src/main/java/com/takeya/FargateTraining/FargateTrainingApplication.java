0d5a8645614c8d15007c49f2a2b12add163515d0
tkkj0017 <tttvvvaken@gmail.com>
2022-02-03 22:38:59 +0900

    first

---
+++ src/main/java/com/takeya/FargateTraining/FargateTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.takeya.FargateTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FargateTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FargateTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takeya/FargateTraining/FargateTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeya.FargateTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FargateTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
